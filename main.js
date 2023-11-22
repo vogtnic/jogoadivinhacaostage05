79e0b0d1a4c4377a69f15590936e03540249ec9d
Nicolas Vogt <100420472+vogtnic@users.noreply.github.com>
2023-11-22 13:20:17 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,47 @@
+// variáveis
+let randomNumber = Math.round(Math.random() * 10)
+let xAttempts = 1
+
+const screen1 = document.querySelector(".screen1")
+const screen2 = document.querySelector(".screen2")
+
+const btnTry = document.querySelector("#btnTry")
+const btnReset = document.querySelector("#btnReset")
+
+//Eventos
+
+btnTry.addEventListener("click", handleTryClick)
+btnReset.addEventListener("click", handleResetClick)
+document.addEventListener("keydown", function (e) {
+  if (e.key == "Enter" && screen1.classList.contains("hide")) {
+    handleResetClick()
+  }
+})
+
+// functions
+
+function handleTryClick(event) {
+  event.preventDefault() //não faça o padrão
+
+  const inputNumber = document.querySelector("#inputNumber")
+
+  if (Number(inputNumber.value) == randomNumber) {
+    toggleScreen()
+    screen2.querySelector(
+      "h2"
+    ).innerText = `Acertou em ${xAttempts} tentativas!`
+  }
+  inputNumber.value = ""
+  xAttempts++
+}
+
+function handleResetClick() {
+  toggleScreen()
+  xAttempts = 1
+  randomNumber = Math.round(Math.random() * 10)
+}
+
+function toggleScreen() {
+  screen1.classList.toggle("hide")
+  screen2.classList.toggle("hide")
+}
